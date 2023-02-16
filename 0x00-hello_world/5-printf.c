953344b0b4f27ddfecc2828781484ed62389c7ab
Only1allan <kariukiallan850@gmail.com>
2023-02-16 06:55:49 -0800

    updated the 1-complier script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
+/**
+ * This is a code that prints a string to standard output.
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,'\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main -Entry 
+ * a function that prints the size of a data type in bytes
+ */
+
 int main(void)
 {
-int x
-char y;
-long z;
-double a;
-printf(sizeof(x));
-printf(sizeof(y));
-printf(sizeof(z));
-printf(sizeof(a));
+printf("The size of char : %c byte(s)\n", sizeof(char));
+printf("The size of int : %d byte(s)\n", sizeof(int));
+printf("The size of long : %li byte(s)\n", sizeof(long));
+printf("The size of double : %lf byte(s)\n", sizeof(double));
 return (0);
 }
