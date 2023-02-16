7a8a3acc1eb12ce61407ce64327c48bbb600406a
Only1allan <kariukiallan850@gmail.com>
2023-02-16 06:12:53 -0800

    added hello world scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
+/*
+ *  This is a function that prints an array of characters
+ */
 int main(void)
 {
-	char my_str [] = '"Programming is like building a multilingual puzzle ';
-	puts(my_str);
-	return (0);
+char my_str[] = "\"Programming is like building a multilingual puzzle";
+puts(my_str);
+return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+int x
+char y;
+long z;
+double a;
+printf(sizeof(x));
+printf(sizeof(y));
+printf(sizeof(z));
+printf(sizeof(a));
+return (0);
+}
