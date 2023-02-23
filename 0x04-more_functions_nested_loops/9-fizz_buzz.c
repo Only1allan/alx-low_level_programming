67b63bff57a0795271869e6d1e1d504059e23447
Only1allan <kariukiallan850@gmail.com>
2023-02-23 14:38:04 -0800

    fixed fizzbuzz script and removed whitespaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,19 +7,24 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <=100; i++)
+	i = 1;
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if ( i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if ( i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", i);
-		printf(" ");
-	}	
+
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
 	printf("\n");
-	return 0;
+
+	return (0);
 }
 
