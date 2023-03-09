04ef93327d11bb604903262f6f114519dc1042e2
Only1allan <kariukiallan850@gmail.com>
2023-03-08 17:47:57 -0800

    fixed bugs in sqrt_recursion and power_recursion functions

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,18 @@
 # include "main.h"
 /**
- * _factorial - a function that return factorial of n
+ * factorial - a function that return factorial of n
  * @n: parameter for the function
  * Return: 0 always a success
  */
 int factorial(int n)
-{	
-	if (n <= 0)
+{
+	if (n < 0)
 	{
 		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
 	}
 	else
 	{
