4a85cecdf39c89a109b03189adc2e19e851e708a
Only1allan <kariukiallan850@gmail.com>
2023-03-20 10:19:35 -0700

    removed all trailing whitespaces

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file 
+ * main - prints the name of the file
  * Return: 0 always a success
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
