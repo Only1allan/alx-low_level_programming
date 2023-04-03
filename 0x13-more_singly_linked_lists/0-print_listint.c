56618e4d4df0dbd474e7473b63688b57d042c2d7
Only1allan <kariukiallan850@gmail.com>
2023-04-03 12:42:47 -0700

    fixed bug

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,13 +3,13 @@
 /**
  * print_listint - a fucntion that prints elements of a node
  * @h: a pointer to the structure list_int_s
- * Return: 0 always a success
+ * Return: length of the list
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t temp;
 
-	for (temp = 0; h; temp++)
+	for (temp = 0; h != NULL; temp++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
