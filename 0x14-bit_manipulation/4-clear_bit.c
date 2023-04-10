02961c03cecb160f943929d4c03622dc72f04bab
Only1allan <kariukiallan850@gmail.com>
2023-04-10 12:32:22 -0700

    add more bit manipulation function

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,10 +3,16 @@
  * clear_bit - a function that sets the value of a bit to 0
  * @n: a pointer to the binary
  * @index: index of each string pointer
- * Return: value of the bit 
+ * Return: value of the bit
  */
-int clear_bit(unsigned long int *n unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
+	{
 		return (-1);
+	}
 
+	*n &= ~(1 << index);
+
+	return (1);
+}
