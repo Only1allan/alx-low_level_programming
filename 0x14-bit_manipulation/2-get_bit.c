1201b0ec469e1ac5242c3ff4ce0c793ec7358e86
Only1allan <kariukiallan850@gmail.com>
2023-04-10 11:15:03 -0700

    fixed 2-get_bit.c file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,5 +10,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (0);
 	else
-		return ((n & (1 << index)) != 0);
+		return ((n & (1 << index)) >> index);
 }
