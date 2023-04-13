80413ae4ad49b8345fcb24c31f0449ef8fff1b5e
Only1allan <kariukiallan850@gmail.com>
2023-04-13 17:23:41 +0300

    added more file i/o functions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,16 +1,29 @@
 #include "main.h"
 /**
  * append_text_to_file - a function that appends text to file
- * @filename: pointer to the file name 
- * @text-content: pointer to the file content
+ * @filename: pointer to the file name
+ * @text_content: pointer to the file content
  * Return: 1 as a success
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	FILE *fd;
+	size_t len;
+	size_t result;
 
-	fd = open(filename, O_);
+	fd = fopen(filename, "a");
 
-	if (fd == -1)
+	if (fd == NULL)
 		return (-1);
+
+	len = strlen(text_content);
+	result = fwrite(text_content, sizeof(char), len, fd);
+	fclose(fd);
+
+	if (result != len)
+	{
+		return (-1);
+	}
+	return (1);
+}
 
