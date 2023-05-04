1df2195934e68f65790f9d6b3480493f29bd5552
Only1allan <kariukiallan850@gmail.com>
2023-05-04 13:13:01 +0300

    fixed task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,14 +6,13 @@
  */
 void print_binary(unsigned long int n)
 {
-    int i, shift;
-    for (i = 15; i >= 0; i--)
-    {
-       shift =  n >> i;
-        if (shift & 1)
-            _putchar('1');
-        else
-            _putchar('0');
-    }
+	if (n > 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
 }
 
