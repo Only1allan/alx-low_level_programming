bafef7e6e9557085a98b0e0d13fc58c6b9cf4888
Only1allan <kariukiallan850@gmail.com>
2023-06-15 23:59:18 +0300

    add task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_dnodeint - a function that adds a new node at the begining
+ * @head: a pointer to the head of the dll
+ * @n: n represents value to b inserted
+ *Return: address of the new element or null if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
