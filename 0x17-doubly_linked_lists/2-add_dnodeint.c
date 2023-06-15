d7c1220daeb3ed9c6f15a607b33e2a3fad282887
Only1allan <kariukiallan850@gmail.com>
2023-06-16 00:34:40 +0300

    add task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,10 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	node->n = n;
 	node->prev = NULL;
 	h = *head;
-	
+
 	if (h != NULL)
 	{
-		while(h->prev != NULL)
+		while (h->prev != NULL)
 			h = h->prev;
 
 	}
@@ -28,7 +28,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (h != NULL)
 		h->prev = node;
 
-	*head = node;	
+	*head = node;
 
 	return (node);
 }
