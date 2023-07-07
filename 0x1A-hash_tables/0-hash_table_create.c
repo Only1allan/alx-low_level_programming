aa37340997b12f0a7cb0a93beca89dbd698a6464
Only1allan <kariukiallan850@gmail.com>
2023-07-07 10:59:10 +0300

    add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hashtable
+ * @size: size of the hashtable to be created
+ * Return: pointer to newly created hashtable
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hashtable;
+    unsigned long int i;
+
+    hashtable = malloc(sizeof(hash_table_t));
+    hashtable->array = malloc(sizeof(hash_node_t) * size);
+
+    for (i = 0; i < size; i++)
+    {
+        hashtable->array[i] = NULL;
+    }
+    return hashtable;
+}
