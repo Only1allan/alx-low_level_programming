01aeafd2a6a682839f8d88083541ca574eadfbcb
Only1allan <kariukiallan850@gmail.com>
2023-12-14 01:25:40 +0300

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,23 +8,23 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i = 0, j = size - 1, m;
+	size_t i = 0, j = size - 1, m;
 
-    if (array == NULL)
-        return (-1);
-    while (i <= j)
-    {
-        printf("Searching in array: ");
-        for (m = i; m < j; m++)
-            printf("%d, ", array[m]);
-        printf("%d\n", array[m]);
-        m = (i + j) / 2;
-        if (array[m] < value)
-            i = m + 1;
-        else if (array[m] > value)
-            j = m - 1;
-        else
-            return (m);
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	while (i <= j)
+	{
+		printf("Searching in array: ");
+		for (m = i; m < j; m++)
+			printf("%d, ", array[m]);
+		printf("%d\n", array[m]);
+		m = (i + j) / 2;
+		if (array[m] < value)
+			i = m + 1;
+		else if (array[m] > value)
+			j = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
 }
